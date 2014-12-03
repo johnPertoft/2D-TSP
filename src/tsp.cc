11b7281283464728f09857635ca9cdc6d0bf026b
Johan Stenberg <johanstenberg92@gmail.com>
2014-12-03 16:18:50 +0100

    derp

--- src/tsp.cc
+++ src/tsp.cc
@@ -16,11 +16,13 @@
 
 #define MAX 1000
 
-#define TIME_LIMIT 1600000000 // PROD
+//#define TIME_LIMIT 1600000000 // PROD
 
-//#define TIME_LIMIT 1600000 // MAC
+#define TIME_LIMIT 1600000 // MAC
 
-#define MAX_NN 40
+#define MAX_NN 80
+
+bool dlb[MAX];
 
 using namespace std;
 
@@ -44,22 +46,21 @@ struct nn_sort {
 
 };
 
-void nnList(vector<vector<int>>& nns) {
+void nnList(vector<vector<int>>& nns, int max_nn) {
     int n = nns.size();
-    int nn_ls = nns[0].size();
     for (int i = 0; i < n; ++i) {
         vector<tuple<int, int>> helper(n);
         for (int j = 0; j < n; ++j) {
             if (j != i) {
                 helper[j] = make_tuple(j, ds[i][j]);
             } else {
-                helper[j] = make_tuple(j, 100000000);
+                helper[j] = make_tuple(j, 10000000000);
             }
         }
 
         sort(helper.begin(), helper.end(), nn_sort());
 
-        for (int j = 0; j < nn_ls; ++j) {
+        for (int j = 0; j < max_nn; ++j) {
             nns[i][j] = get<0>(helper[j]);
         }
     }
@@ -70,14 +71,14 @@ void nnList(vector<vector<int>>& nns) {
 
 // Find decent path with nearest neighbor.
 inline void nearest_neighbor_path(int start, vector<int>& path) {
-    const auto& n = path.size();
+    const auto n = path.size();
     list<int> l;
     for (int i = 0; i < n; ++i) l.emplace_back((start+i) % n);
 
     for (int i = 0; i < n; ++i) {
         auto min = l.begin();
         for (auto k = l.begin(); k != l.end(); ++k) {
-            const auto& d1 = ds[path[i-1]][*k],
+            const auto d1 = ds[path[i-1]][*k],
                 d2 = ds[path[i-1]][*min];
             if (d1 < d2) min = k;
         }
@@ -94,56 +95,82 @@ inline void multi_fragment(vector<int>& path) {
     // TODO: later if we have time
 }
 
+inline void rev(vector<int>& path, vector<int>& indices, int s, int e) {
+    int n = path.size();
+    int i = s - 1;
+    int j = e;
+    int diff = -1;
+    if (i + 1 < j) diff = j - (i + 1);
+    else {
+        diff = n - (i + 1) + j + 1;
+    }
+
+    diff = (diff + 1) / 2;
+
+    int start = i + 1;
+    int end = j;
+    int tmp = -1;
+
+    while (diff > 0) {
+        start %= n;
+        end = end == -1 ? n - 1 : end;
+
+        tmp = path[start];
+        path[start] = path[end];
+        path[end] = tmp;
+
+        //indices[path[start]] = start;
+        //indices[path[end]] = end;
+
+        ++start;
+        --end;
+        --diff;
+    }
+
+    for (int i = 0; i < n; ++i) {
+        indices[path[i]] = i;
+    }
+}
+
 
 //////// Local optimization algorithms ///////////
 
-inline bool opt2(vector<int>& path) {
+inline bool opt2_greedy(
+                        vector<int>& path,
+                        vector<int>& indices,
+                        vector<vector<int>>& nns,
+                        int max_nn) {
     const int n = path.size();
     bool improved = true;
     while (improved) {
         improved = false;
 
-        for (int i = 0; i < n; ++i) {
-            int j = (i + 1) % n;
+        for (int i = 0; i < n - 1; ++i) {
+            if (dlb[path[i]]) continue;
 
-            while (i != j) {
+            bool good_i = false;
+
+            for (int k = 0; k < max_nn - 1; ++k) { // nns => vertices
                 const int a = path[i];
-                const int b = path[(i + 1) % n];
-                const int c = path[j];
-                const int d = path[(j + 1) % n];
+                const int b = path[i + 1];
+
+                int vert = nns[a][k];
+
+                int j = indices[vert]; // vertex to index
+
+                const int c = vert; // first vertex
+                const int d = path[(j + 1) % n]; // second vertex
+                if (b == c || b == d) continue;
 
                 if (ds[a][c] + ds[b][d] < ds[a][b] + ds[c][d]) {
+                    good_i = true;
                     improved = true;
 
-                    int diff = -1;
-                    if (i + 1 < j) diff = j - (i + 1);
-                    else {
-                        diff = n - (i + 1) + j + 1;
-                    }
-
-                    diff /= 2;
-
-                    int start = i + 1;
-                    int end = j;
-                    int tmp = -1;
-
-                    while (diff > 0) {
-                        start %= n;
-                        end = end == -1 ? n - 1 : end;
-
-                        tmp = path[start];
-                        path[start] = path[end];
-                        path[end] = tmp;
-
-                        ++start;
-                        --end;
-                        --diff;
-                    }
+                    rev(path, indices, i + 1, j);
                 }
-
-                ++j;
-                j %= n;
             }
+
+            if (!good_i) dlb[path[i]] = true;
         }
 
         if ((chrono::system_clock::now() - tsp_begin).count() > TIME_LIMIT) {
@@ -154,73 +181,107 @@ inline bool opt2(vector<int>& path) {
     return true;
 }
 
-// Returns true if the path was made shorter
-inline bool opt22(vector<int>& path) {
-  bool improved = false;
-  const auto& n = path.size();
-  for (int i = 0; i < n; i++) {
-    for (int j = i+1; j <= n-1; j++) {
-      const auto& a = path[i%n],
-                  b = path[(i+1)%n], // use mod n to wrap around
-                  c = path[j%n],
-                  d = path[(j+1)%n];
-
-      auto current = ds[a][b] + ds[c][d];
-      auto changed = ds[a][c] + ds[b][d];
-
-      if (changed < current) {
-        // TODO: handle case where we wrap around
-        // normal case
-        reverse(path.begin()+i+1, path.begin()+j+1);
-        improved = true;
-      }
+inline void rev_opt3(vector<int>& route, int s, int t) {
+    int length = route.size();
+    for (int i = (t - s) / 2; i >= 0; i--) {
+        int tmp = route[(s + i) % length];
+        route[(s + i) % length] = route[(t - i) % length];
+        route[(t - i) % length] = tmp;
     }
-  }
-
-  return improved;
 }
 
-// Optimize path by exchanging edge triplets with shorter edge triplets.
-inline void opt3(vector<int>& path) {
-    const auto& n = path.size();
-    for (int i = 0; i < n-1; ++i) {
-        for (int j = i+1; j < n-1; ++j) {
-            for (int k = j+1; k < n-1; ++k) {
-                auto& a = path[i],
-                    b = path[i+1],
-                    c = path[j],
-                    d = path[j+1],
-                    e = path[k],
-                    f = path[k+1];
-                //TODO Try the other way of combining also. ABCDEF -> ADECBF
-                if (ds[a][b] + ds[c][d] + ds[e][f] > ds[a][d] + ds[b][e] + ds[c][f]) {
-                    // Reverse path between b and c.
-                    reverse(path.begin()+i+1, path.begin()+j+1);
-                    swap(b, d);
-                    swap(c, f);
+
+bool opt3(vector<int>& p) {
+    int n = p.size();
+
+    for (int i = 1; i < n - 1; ++i) {
+        int s1 = p[i - 1];
+        int t1 = p[i];
+
+        int d1 = ds[s1][t1];
+
+        for (int j = i + 1; j < n; ++j) {
+            int s2 = p[j - 1];
+            int t2 = p[j];
+
+            int d2 = ds[s2][t2];
+
+            for (int k = j + 1; k <= n; ++k) {
+                if ((k + 2) % n == j || (j + 2) % n == i || i + 2 == k)
+                    continue;
+
+                int s3 = p[k - 1];
+                int t3 = p[k % n];
+
+                int before = d1 + d2 + ds[s3][t3];
+                int after = ds[s1][t2] + ds[s3][t1] + ds[s2][t3];
+
+                if (before > after) {
+                    rev_opt3(p, i, j - 1);
+                    rev_opt3(p, j, k - 1);
+                    rev_opt3(p, i, k - 1);
+                    return true;
                 }
+
+                after = ds[s1][t2] + ds[s3][s2] + ds[t1][t3];
+
+                if (before > after) {
+                    rev_opt3(p, j, k - 1);
+                    rev_opt3(p, i, k - 1);
+                    return true;
+                }
+
+                after = ds[s1][s3] + ds[t1][t2] + ds[s2][t3];
+
+                if (before > after) {
+                    rev_opt3(p, i, j - 1);
+                    rev_opt3(p, i, k - 1);
+                    return true;
+                }
+
+                after = ds[s1][s2] + ds[t1][s3] + ds[t2][t3];
+
+                if (before > after) {
+                    rev_opt3(p, i, j - 1);
+                    rev_opt3(p, j, k - 1);
+                    return true;
+                }
+
+            }
+
+            if ((chrono::system_clock::now() - tsp_begin).count() > TIME_LIMIT) {
+                return false;
             }
         }
     }
+
+    return false;
 }
 
-inline void opt33(vector<int>& path) {
-    const auto n = path.size();
+// Perform a random double bridge move.
+inline void perturb(vector<int>& p, vector<int>& indices) {
+    const auto n = p.size();
+    if (n < 8) return;
 
-    for (int i = 0; i < n-5; ++i) {
-        for (int j = i+2; j < n-3; ++j) {
-            for (int k = j+2; k < n-1; ++k) {
-                const auto a = path[i],
-                    b = path[i+1],
-                    c = path[j],
-                    d = path[j+1],
-                    e = path[k],
-                    f = path[k+1];
+    static random_device rd;
+    static uniform_int_distribution<int> d(2, n - 3);
+    static auto r = bind(d, ref(rd));
 
-                //if (ds[a][b] + ds[c][d] + ds[e][f] <
-            }
-        }
-    }
+    auto pos = r();
+
+    int s1 = pos - 1;
+    int s2 = pos + 1;
+
+    int tmp = p[s1];
+    p[s1] = p[s2];
+    p[s2] = tmp;
+
+    indices[p[s2]] = s2;
+    indices[p[s1]] = s1;
+
+
+    dlb[s1 - 1] = false;
+    dlb[s2 + 1] = false;
 }
 
 int main() {
@@ -245,13 +306,50 @@ int main() {
     if (n >= 10) greedy(path, ds);
     else nearest_neighbor_path(0, path);
 
-    opt2(path); // Opt 2 until time limit or no improvement
+    int max_nn = min(MAX_NN, n);
+    vector<vector<int>> nns(n, vector<int>(max_nn));
+    nnList(nns, max_nn);
 
-    for (const auto v : path) cout << v << endl;
+    vector<int> indices(n);
+    for (int i = 0; i < n; ++i) {
+        indices[path[i]] = i;
+    }
+
+    fill_n(dlb, n, false);
+    opt2_greedy(path, indices, nns, max_nn);
+
+    int best = length(path);
+    vector<int> bestPath = path;
+
+    int iters = 0;
+
+    while (true) {
+        ++iters;
+
+        perturb(path, indices); // Perturb that stuffs
+
+        bool tle = opt2_greedy(path, indices, nns, max_nn);
+
+        int l = length(path);
+
+        cout << l << endl;
+
+        if (l < best) {
+            bestPath = path;
+            best = l;
+        }
+
+        if (!tle) {
+            break;
+        }
+    }
+
+    //for (const auto v : bestPath) cout << v << endl;
 
     // Use this pattern to print debug prints without breaking kattis
 #ifdef VERBOSE
-    cout << "length: " << length(path) << endl;
+    cout << "length: " << length(bestPath) << endl;
+    cout << "iters: " << iters << endl;
 #endif
 
     return 0;
